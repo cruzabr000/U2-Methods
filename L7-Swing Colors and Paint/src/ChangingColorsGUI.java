b5bc736396f7f567263c08bf2f888f93fab7e6ca
Cruzabr000 <cruzabr000@warren.k12.in.us>
2021-10-22 11:47:13 -0400

    U2-1 fettttometers

--- L7-Swing Colors and Paint/src/ChangingColorsGUI.java
+++ L7-Swing Colors and Paint/src/ChangingColorsGUI.java
@@ -1,24 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class ChangingColorsGUI {
 
     //Variables in a class are declared here,
     //but often not initialized
 
-    JFrame window;
-    JPanel panel;
-    JButton colorClicker;
 
     /*
     This is a constructor and absolutely necessary for a class.
     This is where we will build the GUI, it must have the same
     name as the class.
-    */
 
-    public ChangingColorsGUI(){
 
-        window = new JFrame("Color Changer");
+
+
+
+
+
+
+
+
+        JFrame window;
+    JPanel panel;
+    JButton colorClicker;
+    Color panelBackground = new Color(33, 33, 33);
+    Color ButtonForeground = new Color(202, 217, 179);
+    Color buttonBackground = new Color(109, 152, 134);
+
+
+
+
+
+
+            window = new JFrame("Color Changer");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(340, 127);
         window.setLocationRelativeTo(null);
@@ -27,8 +43,12 @@ public class ChangingColorsGUI {
         colorClicker = new JButton("Change the Colors");
 
         //create a new Custom color
+colorClicker.addActionListener(new ColorChanger());
 
         //Set the colors of our buttons and panel
+        panel.setBackground(Color.CYAN);
+        colorClicker.setForeground(Color.ORANGE);
+        colorClicker.setBackground(buttonBackground);
 
         panel.add(colorClicker);
         window.add(panel);
@@ -37,8 +57,17 @@ public class ChangingColorsGUI {
     }
 
     //Add a listener to change the color when the button is clicked
+    private class ColorChanger implements ActionListener) {
+    @Override
+            public void actionPreformed(ActionEvent e){
+        panel.setBackground(buttonBackground);
+        colorClicker.setForeground(buttonBackground);
+        colorClicker.setBackground(panelBackground);
+        }
+    }
 
 
-
-
+     */
 }
+
+
--- L7-Swing Colors and Paint/src/ColorSwitchGUI.java
+++ L7-Swing Colors and Paint/src/ColorSwitchGUI.java
@@ -1,11 +1,77 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class ColorSwitchGUI {
 
     //Declare your frame, panel and three buttons.
+    JFrame window = new JFrame("Color switcher");
+    JPanel panel = new JPanel();
+    JButton ColorThemeDark = new JButton("Theme; Dark");
+    JButton ColorThemeWarren = new JButton("Theme; Warren");
+    JButton ColorThemePrimary = new JButton("Theme; Primary");
+
     public ColorSwitchGUI(){
         //create your GUI
+
+
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(400, 300);
+        window.setLocationRelativeTo(null);
+
+        panel.add(ColorThemeDark);
+        panel.add(ColorThemeWarren);
+        panel.add(ColorThemePrimary);
+
+        window.add(panel);
+        window.setVisible(true);
+
+        ColorThemeDark.addActionListener(new ThemeDark());
+        ColorThemeDark.addActionListener(new Themewarren());
+        ColorThemeDark.addActionListener(new ThemePrimary());
+
     }
 
     //add listeners to change the colors. Don't forget to link them to buttons.
+    private class ThemeDark implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Color darkPanel = new Color(25,26,25);
+            Color darkButton = new Color(30,81,40);
+            Color darkText = new Color(216,233,168);
+
+            panel.setBackground(darkPanel);
+            ColorThemeDark.setBackground(darkButton);
+            ColorThemeDark.setBackground(darkText);
+
+
+        }
+    }
+    private class Themewarren implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Color warrenPanel = new Color(0,0,0);
+            Color warrenButton = new Color(240,160,0);
+            Color darkText = new Color(244,244,244);
+
+            panel.setBackground(warrenPanel);
+            ColorThemeDark.setBackground(warrenButton);
+            ColorThemeDark.setBackground(darkText);
+
+
+        }
+    }
+    private class ThemePrimary implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            panel.setBackground(Color.BLUE);
+            ColorThemeDark.setBackground(Color.YELLOW);
+            ColorThemeDark.setBackground(Color.BLUE);
+
+
+        }
+    }
 }
